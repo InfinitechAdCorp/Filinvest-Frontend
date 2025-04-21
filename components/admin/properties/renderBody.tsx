b16007036a3bacd94d9233b8336e64aebe479c6a
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-04-21 11:20:41 +0800

    update

--- components/admin/properties/renderBody.tsx
+++ components/admin/properties/renderBody.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { TableRow, TableCell, Image } from "@heroui/react";
 import { Column } from "@/types/globals";
 import {
