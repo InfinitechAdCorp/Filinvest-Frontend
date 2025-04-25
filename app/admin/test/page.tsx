2063055dad33bef071ca79d0eaaad6d777870304
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-04-25 09:38:03 +0800

    test lightgallery

--- app/admin/test/page.tsx
+++ app/admin/test/page.tsx
@@ -1,118 +1,31 @@
 "use client";
 
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+import "lightgallery/css/lg-autoplay.css";
+import "lightgallery/css/lg-share.css";
+import "lightgallery/css/lg-rotate.css";
+
 import React from "react";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableColumn,
-  TableHeader,
-  TableRow,
-} from "@heroui/react";
+import LightGallery from "lightgallery/react/Lightgallery.es5";
+import lgThumnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+import lgAutoplay from "lightgallery/plugins/autoplay";
+import lgVideo from "lightgallery/plugins/video";
+import lgShare from "lightgallery/plugins/share";
+import lgRotate from "lightgallery/plugins/rotate";
 
 const Page = () => {
   return (
-    <div className="w-full mx-96">
-      <Table aria-label="Example static collection table">
-        <TableHeader>
-          <TableColumn>1</TableColumn>
-          <TableColumn>2</TableColumn>
-          <TableColumn>3</TableColumn>
-          <TableColumn>4</TableColumn>
-          <TableColumn>5</TableColumn>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell className="max-h-[1rem] max-w-[30rem] overflow-hidden whitespace-nowrap text-ellipsis">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-              optio dolorum nisi nulla, aut quod? Nulla, a? Eveniet, eum
-              similique quibusdam, et nulla iure cumque repellendus
-              reprehenderit dicta porro accusamus in dolorum quam placeat atque.
-              Numquam dolorum consectetur iste quos dicta error minus dolore,
-              deserunt dignissimos molestias, laboriosam reprehenderit sapiente,
-              neque nam quas voluptas velit accusamus? Non optio voluptates
-              iusto, repellat recusandae dolor culpa esse perspiciatis eligendi,
-              aut quaerat ad amet quisquam ratione rem quos, veniam totam
-              repellendus placeat debitis dolorem? Accusantium assumenda,
-              nostrum nisi ad hic, officiis recusandae sit amet enim eveniet
-              fuga ipsum id est ut nam optio provident a corporis at praesentium
-              facilis! Repudiandae molestias, delectus labore quod commodi
-              tempore.
-            </TableCell>
-            <TableCell>2</TableCell>
-            <TableCell>3</TableCell>
-            <TableCell>4</TableCell>
-            <TableCell>5</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-              optio dolorum nisi nulla, aut quod? Nulla, a? Eveniet, eum
-              similique quibusdam, et nulla iure cumque repellendus
-              reprehenderit dicta porro accusamus in dolorum quam placeat atque.
-              Numquam dolorum consectetur iste quos dicta error minus dolore,
-              deserunt dignissimos molestias, laboriosam reprehenderit sapiente,
-              neque nam quas voluptas velit accusamus? Non optio voluptates
-              iusto, repellat recusandae dolor culpa esse perspiciatis eligendi,
-              aut quaerat ad amet quisquam ratione rem quos, veniam totam
-              repellendus placeat debitis dolorem? Accusantium assumenda,
-              nostrum nisi ad hic, officiis recusandae sit amet enim eveniet
-              fuga ipsum id est ut nam optio provident a corporis at praesentium
-              facilis! Repudiandae molestias, delectus labore quod commodi
-              tempore.
-            </TableCell>
-            <TableCell>2</TableCell>
-            <TableCell>3</TableCell>
-            <TableCell>4</TableCell>
-            <TableCell>5</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-              optio dolorum nisi nulla, aut quod? Nulla, a? Eveniet, eum
-              similique quibusdam, et nulla iure cumque repellendus
-              reprehenderit dicta porro accusamus in dolorum quam placeat atque.
-              Numquam dolorum consectetur iste quos dicta error minus dolore,
-              deserunt dignissimos molestias, laboriosam reprehenderit sapiente,
-              neque nam quas voluptas velit accusamus? Non optio voluptates
-              iusto, repellat recusandae dolor culpa esse perspiciatis eligendi,
-              aut quaerat ad amet quisquam ratione rem quos, veniam totam
-              repellendus placeat debitis dolorem? Accusantium assumenda,
-              nostrum nisi ad hic, officiis recusandae sit amet enim eveniet
-              fuga ipsum id est ut nam optio provident a corporis at praesentium
-              facilis! Repudiandae molestias, delectus labore quod commodi
-              tempore.
-            </TableCell>
-            <TableCell>2</TableCell>
-            <TableCell>3</TableCell>
-            <TableCell>4</TableCell>
-            <TableCell>5</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-              optio dolorum nisi nulla, aut quod? Nulla, a? Eveniet, eum
-              similique quibusdam, et nulla iure cumque repellendus
-              reprehenderit dicta porro accusamus in dolorum quam placeat atque.
-              Numquam dolorum consectetur iste quos dicta error minus dolore,
-              deserunt dignissimos molestias, laboriosam reprehenderit sapiente,
-              neque nam quas voluptas velit accusamus? Non optio voluptates
-              iusto, repellat recusandae dolor culpa esse perspiciatis eligendi,
-              aut quaerat ad amet quisquam ratione rem quos, veniam totam
-              repellendus placeat debitis dolorem? Accusantium assumenda,
-              nostrum nisi ad hic, officiis recusandae sit amet enim eveniet
-              fuga ipsum id est ut nam optio provident a corporis at praesentium
-              facilis! Repudiandae molestias, delectus labore quod commodi
-              tempore.
-            </TableCell>
-            <TableCell>2</TableCell>
-            <TableCell>3</TableCell>
-            <TableCell>4</TableCell>
-            <TableCell>5</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </div>
+    <LightGallery
+      speed={500}
+      plugins={[lgThumnail, lgZoom, lgAutoplay, lgVideo, lgShare, lgRotate]}
+    >
+      <a href="/images/logo.png">
+        <img src="/images/logo.png" alt="test" />
+      </a>
+    </LightGallery>
   );
 };
 
