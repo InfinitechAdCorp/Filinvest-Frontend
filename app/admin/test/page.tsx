ddda6fa66f0b176e40022ca44a23c9d1047c1b65
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-04-25 09:54:22 +0800

    gallery global component

--- app/admin/test/page.tsx
+++ app/admin/test/page.tsx
@@ -1,32 +1,9 @@
 "use client";
 
-import "lightgallery/css/lightgallery.css";
-import "lightgallery/css/lg-zoom.css";
-import "lightgallery/css/lg-thumbnail.css";
-import "lightgallery/css/lg-autoplay.css";
-import "lightgallery/css/lg-share.css";
-import "lightgallery/css/lg-rotate.css";
-
 import React from "react";
-import LightGallery from "lightgallery/react/Lightgallery.es5";
-import lgThumnail from "lightgallery/plugins/thumbnail";
-import lgZoom from "lightgallery/plugins/zoom";
-import lgAutoplay from "lightgallery/plugins/autoplay";
-import lgVideo from "lightgallery/plugins/video";
-import lgShare from "lightgallery/plugins/share";
-import lgRotate from "lightgallery/plugins/rotate";
 
 const Page = () => {
-  return (
-    <LightGallery
-      speed={500}
-      plugins={[lgThumnail, lgZoom, lgAutoplay, lgVideo, lgShare, lgRotate]}
-    >
-      <a href="/images/logo.png">
-        <img src="/images/logo.png" alt="test" />
-      </a>
-    </LightGallery>
-  );
+  return <h3>1</h3>;
 };
 
 export default Page;
---
+++ components/globals/gallery.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+import "lightgallery/css/lg-autoplay.css";
+import "lightgallery/css/lg-share.css";
+import "lightgallery/css/lg-rotate.css";
+
+import React from "react";
+import LightGallery from "lightgallery/react/Lightgallery.es5";
+import Thumnail from "lightgallery/plugins/thumbnail";
+import Zoom from "lightgallery/plugins/zoom";
+import Autoplay from "lightgallery/plugins/autoplay";
+import Video from "lightgallery/plugins/video";
+import Share from "lightgallery/plugins/share";
+import Rotate from "lightgallery/plugins/rotate";
+
+type Props = {
+  images: {
+    url: string;
+    name: string;
+  }[];
+};
+
+const Gallery = ({ images }: Props) => {
+  return (
+    <LightGallery
+      speed={500}
+      plugins={[Thumnail, Zoom, Autoplay, Video, Share, Rotate]}
+    >
+      {images.map((image) => (
+        <a href={image.url}>
+          <img src={image.url} alt={image.name} />
+        </a>
+      ))}
+    </LightGallery>
+  );
+};
+
+export default Gallery;
