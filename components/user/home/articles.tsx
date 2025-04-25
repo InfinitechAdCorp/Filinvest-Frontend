74ad5be6d1ed9ef6c89234aab4b76e8f5935223e
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-04-25 11:22:13 +0800

    optimize article modal

--- components/user/home/articles.tsx
+++ components/user/home/articles.tsx
@@ -6,14 +6,11 @@ import {
   CardBody,
   CardHeader,
   Image,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalHeader,
   useDisclosure,
 } from "@heroui/react";
 import { Article } from "@/types/globals";
 import { formatDate } from "@/utils/formatters";
+import ArticleModal from "./articleModal";
 
 type Props = {
   articles: Article[];
@@ -69,48 +66,7 @@ const Articles = ({ articles }: Props) => {
         </div>
       </div>
 
-      <Modal
-        size="3xl"
-        isOpen={isOpen}
-        onClose={onClose}
-        scrollBehavior="inside"
-      >
-        <ModalContent>
-          {() => (
-            <>
-              <ModalHeader className="flex flex-col">
-                <h3 className="text-2xl font-bold text-primary">
-                  {selected!.name}
-                </h3>
-                <p className="text-sm text-primary">
-                  {selected?.date ? formatDate(selected.date) : ""}
-                </p>
-              </ModalHeader>
-
-              <ModalBody className="p-5">
-                <div className="flex flex-col gap-4">
-                  {selected?.image ? (
-                    <Image
-                      isBlurred
-                      alt="Article"
-                      className="w-full h-[25rem] object-cover rounded-xl"
-                      src={`${process.env.NEXT_PUBLIC_S3_URL}/articles/${selected.image}`}
-                    />
-                  ) : (
-                    <p>No media available</p>
-                  )}
-
-                  <div className="max-w-none">
-                    <p className="text-gray-700 leading-relaxed">
-                      {selected?.description}
-                    </p>
-                  </div>
-                </div>
-              </ModalBody>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+      <ArticleModal selected={selected} isOpen={isOpen} onClose={onClose} />
     </>
   );
 };
